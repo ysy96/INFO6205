86008d56ee2b1d5d1308abfbf17e5e54ba2d97b9
ysy96 <kathyyue123@hotmail.com>
2020-10-23 18:28:50 +0800

    Modify benchmark tests

--- src/main/java/edu/neu/coe/info6205/union_find/UFAlternatives.java
+++ src/main/java/edu/neu/coe/info6205/union_find/UFAlternatives.java
@@ -26,13 +26,13 @@ public class UFAlternatives {
     /**
      * Getting input from users to determine the number of sites
      * and print out the connections generated using count() method
+     * Benchmark to see the execution time
      * @param args
      */
     public static void main(String args[]){
         int n = Integer.parseInt(args[0]);
-        System.out.println(count(n)+" connections generated");
         long startTime = System.currentTimeMillis();
-        count(n);
+        System.out.println(count(n)+" connections generated");
         long endTime = System.currentTimeMillis();
         System.out.println("Total execution time: " + (endTime-startTime) + "ms");
     }
--- src/main/java/edu/neu/coe/info6205/union_find/UFClient.java
+++ src/main/java/edu/neu/coe/info6205/union_find/UFClient.java
@@ -49,9 +49,8 @@ public class UFClient{
      */
     public static void main(String args[]){
         int n = Integer.parseInt(args[0]);
-        System.out.println(count(n)+" connections generated");
         long startTime = System.currentTimeMillis();
-        count(n);
+        System.out.println(count(n)+" connections generated");
         long endTime = System.currentTimeMillis();
         System.out.println("Total execution time: " + (endTime-startTime) + "ms");
 
